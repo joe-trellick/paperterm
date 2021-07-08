b4572c2c380807e0f404b46bfb4813c7a791f4e8
Joe Trellick <joe@trellick.work>
2021-07-08 19:14:04 +0100

    Support return key in input field

--- clientjs/main.ts
+++ clientjs/main.ts
@@ -1,7 +1,23 @@
 console.log("main.ts is loaded");
 
-function goButton() {
-    let inputField:HTMLInputElement = document.getElementById("command") as HTMLInputElement;
+var inputField:HTMLInputElement;
+
+// Wait for DOM and then attach handlers
+document.addEventListener("DOMContentLoaded", () => {
+    attachEventListeners();
+});
+
+function attachEventListeners() {
+    // Add return key support for input
+    inputField = document.getElementById("command") as HTMLInputElement;
+    inputField.addEventListener("keyup", ({key}) => {
+        if (key === "Enter") {
+            sendCommand();
+        }
+    });
+}
+
+function sendCommand() {
     let input:string = inputField.value;
     console.log(`The input is "${input}"`);
 }
