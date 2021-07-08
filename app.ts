e892a0bb979cca6cc92368675c4d205296cb2230
Joe Trellick <joe@trellick.work>
2021-07-08 20:04:51 +0100

    Make sending shell commands actually work

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import * as child from 'child_process';
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static('public'));
 app.use(express.static('build/clientjs'));
 
@@ -14,6 +15,21 @@ app.get('/ls', (req, res) => {
     child.exec("ls", (error, stdout, stderr) => {
         res.send(stdout);
     })
+})
+
+app.post('/command', (req: express.Request, res: express.Response) => {
+    let command = req.body.command
+    console.log(`Got /command request: "${command}"`)
+    var result: string;
+    try {
+        // TODO: this is blocking, won't be any good for non-instant commands!
+        result = child.execSync(command).toString()
+    } catch (error) {
+        result = error.stderr.toString()
+    }
+    let responseObject = {command: command, output: result}
+    res.send(JSON.stringify(responseObject));
+    console.log(`Result is: "${result}" --> "${JSON.stringify(responseObject)}"`);
 })
 
 app.listen(3000, () => {
--- clientjs/main.ts
+++ clientjs/main.ts
@@ -1,13 +1,14 @@
 console.log("main.ts is loaded");
 
 var inputField:HTMLInputElement;
+var outputField:HTMLTextAreaElement;
 
 // Wait for DOM and then attach handlers
 document.addEventListener("DOMContentLoaded", () => {
-    attachEventListeners();
+    setupDOMElements();
 });
 
-function attachEventListeners() {
+function setupDOMElements() {
     // Add return key support for input
     inputField = document.getElementById("command") as HTMLInputElement;
     inputField.addEventListener("keyup", ({key}) => {
@@ -15,9 +16,26 @@ function attachEventListeners() {
             sendCommand();
         }
     });
+
+    outputField = document.getElementById("output") as HTMLTextAreaElement
 }
 
 function sendCommand() {
     let input:string = inputField.value;
     console.log(`The input is "${input}"`);
+    fetch('/command', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({command: input})
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(`Response: ${JSON.stringify(data)}`)
+        outputField.value = data.output
+    })
+    .catch((error) => {
+        console.error('Error:', error)
+    });
 }
